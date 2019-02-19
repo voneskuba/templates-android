5c533dbedd773017941f4ec8dae5df92afb1727c
Jakub Voneš <jakub.vones@thefuntasty.com>
2019-02-19 13:05:16 +0100

    Create resource loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 }
 
 group = "com.github.voneskuba"
-version = "0.1.4"
+version = "0.1.5"
 
 allprojects {
     repositories {
--- src/main/java/org/vones/templates/Templates.java
+++ src/main/java/org/vones/templates/Templates.java
@@ -4,6 +4,8 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.Copy;
 
+import java.util.Objects;
+
 public class Templates implements Plugin<Project> {
 
     private static final String FROM = "%s/templates";
@@ -11,7 +13,11 @@ public class Templates implements Plugin<Project> {
 
     @Override public void apply(Project project) {
         project.getTasks().create("copyTemplates", Copy.class, task -> {
-            String from = String.format(FROM, project.getRootDir());
+            String from = String.format(FROM, project.fileTree(
+                    Objects.requireNonNull(
+                            getClass().getClassLoader().getResource("/templates"))
+                    )
+            );
             task.from(from, t -> t.include("**/"));
             task.into(INTO);
             task.doLast(t -> {
