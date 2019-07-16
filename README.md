fc1709def32f1a5d36118a3e072c0f58801d6a78
Jakub Voneš <jakub.vones@thefuntasty.com>
2019-07-17 00:53:06 +0200

    Bump version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Add to plugins:
 ```
-id("com.github.voneskuba.templates-android") version "0.5.2"
+id("com.github.voneskuba.templates-android") version "0.5.3"
 ```
 
 Add to settings.gradle.kts:
--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -1,7 +1,7 @@
 object ProjectSettings {
 
     const val group = "com.github.voneskuba"
-    const val version = "0.5.2"
+    const val version = "0.5.3"
 
     object Templates {
         const val module = "templates"
