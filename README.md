2986715ac884e713919b02349802495c98caf69a
Jakub Voneš <jakub.vones@thefuntasty.com>
2019-07-26 12:29:55 +0200

    Added new version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Add to plugins:
 ```
-id("com.github.voneskuba.templates-android") version "0.7.0"
+id("com.github.voneskuba.templates-android") version "0.8.0"
 ```
 
 Add to settings.gradle.kts:
--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -1,7 +1,7 @@
 object ProjectSettings {
 
     const val group = "com.github.voneskuba"
-    const val version = "0.7.0"
+    const val version = "0.8.0"
 
     object Templates {
         const val module = "templates-android"
